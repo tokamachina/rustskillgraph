876dd20d9e512eb802f7972950b44f98ede8f9d2
tokamachina <tokamachina@gmail.com>
2024-07-17 14:06:37 -0400

    creating functions for u32 and f32 reading

--- src/main.rs
+++ src/main.rs
@@ -5,87 +5,42 @@ fn main() {
     let bytes = include_bytes!("passiveskillgraph.psg");
     let mut br = Reader::new(bytes);
     br.advance_by(54);
-    let group_count = u32::from_le_bytes(
-        [br.next().unwrap(),
-        br.next().unwrap(),
-        br.next().unwrap(),
-        br.next().unwrap()]
-    );
+    let group_count = get_next_as_u32(&mut br);
     println!("groups: {}", group_count);
 
     let mut groups: Vec<PsgGroup> = Vec::new();
 
     for _i in 0..group_count {
-        let xloc = f32::from_le_bytes(
-            [br.next().unwrap(),
-            br.next().unwrap(),
-            br.next().unwrap(),
-            br.next().unwrap()]
-        );
+        let xloc = get_next_as_f32(&mut br);
         println!("xloc: {}", &xloc);
 
-        let yloc: f32 = f32::from_le_bytes(
-            [br.next().unwrap(),
-            br.next().unwrap(),
-            br.next().unwrap(),
-            br.next().unwrap()]
-        );
+        let yloc: f32 = get_next_as_f32(&mut br);
         println!("yloc: {}", &yloc);
 
         br.advance_by(9);
 
-        let node_count = u32::from_le_bytes(
-            [br.next().unwrap(),
-            br.next().unwrap(),
-            br.next().unwrap(),
-            br.next().unwrap()]
-        );
+        let node_count = get_next_as_u32(&mut br);
         println!("nodes: {}", &node_count);
 
         let mut nodes: Vec<PsgNode> = Vec::new();
 
         for _j in 0..node_count {
-            let node_id = u32::from_le_bytes(
-                [br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap()]
-            );
+            let node_id = get_next_as_u32(&mut br);
             println!("node id: {}", &node_id);
 
-            let orbit = u32::from_le_bytes(
-                [br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap()]
-            );
+            let orbit = get_next_as_u32(&mut br);
             println!("orbit: {}", &orbit);
 
-            let orbit_index = u32::from_le_bytes(
-                [br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap()]
-            );
+            let orbit_index = get_next_as_u32(&mut br);
             println!("orbit index: {}", &orbit_index);
 
-            let edge_count = u32::from_le_bytes(
-                [br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap(),
-                br.next().unwrap()]
-            );
+            let edge_count = get_next_as_u32(&mut br);
             println!("edge count: {}", &edge_count);
 
             let mut edges: Vec<u32> = Vec::new();
 
             for _k in 0..edge_count {
-                let new_edge: u32 = u32::from_le_bytes(
-                    [br.next().unwrap(),
-                    br.next().unwrap(),
-                    br.next().unwrap(),
-                    br.next().unwrap()]
-                );
+                let new_edge: u32 = get_next_as_u32(&mut br);
                 println!("new edge: {}", &new_edge);
                 edges.push(new_edge);
                 if _k > 20 {break};
@@ -128,46 +83,6 @@ struct PsgGroup {
     xloc: f32,
     yloc: f32,
     nodes: Vec<PsgNode>
-}
-
-
-fn find_instances(byte_one: u8, byte_two: u8) -> Option<Vec<usize>> {
-    let bytes = include_bytes!("passiveskillgraph.psg");
-    let mut br = Reader::new(bytes);
-    let mut inds: Vec<usize> = Vec::new();
-    while let Some(byte) = br.next() {
-        if byte == byte_one {
-            if let Some(following_byte) = br.next() {
-                if following_byte == byte_two {
-                    inds.push(br.index-1);
-                    println!("found byte sequence");
-                }
-            } else {
-                break;
-            }
-        };
-    }
-    println!("{:?}", inds);
-    match inds.is_empty() {
-        false => Some(inds),
-        _ => None,
-    }
-}
-
-fn print_instances(byte_one: u8, byte_two: u8) {
-    let bytes = include_bytes!("passiveskillgraph.psg");
-    let mut br = Reader::new(bytes);
-    br.read_until(vec![byte_one, byte_two]);
-    for _i in 0..32 {
-        let dat = [
-            br.next().unwrap(), 
-            br.next().unwrap(), 
-            br.next().unwrap(), 
-            br.next().unwrap()
-            ];
-        println!("{:02X?}", dat);
-    }
-    println!("");
 }
 
 fn get_next_as_u32(br: &mut Reader) -> u32 {
