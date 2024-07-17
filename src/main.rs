f593acb48c3199cecd466da0c1c555c7f936a252
tokamachina <tokamachina@gmail.com>
2024-07-17 14:59:09 -0400

    write to file instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,50 @@
 extern crate byte_reader;
 use byte_reader::Reader;
+use std::io::Write;
+use std::fs::File;
 
 fn main() {
+    let mut output = File::create("parsed_psg.txt").unwrap();
     let bytes = include_bytes!("passiveskillgraph.psg");
     let mut br = Reader::new(bytes);
     br.advance_by(54);
     let group_count = get_next_as_u32(&mut br);
-    println!("groups: {}", group_count);
+    writeln!(&mut output, "groups: {}", group_count).unwrap();
 
     let mut groups: Vec<PsgGroup> = Vec::new();
 
     for _i in 0..group_count {
         let xloc = get_next_as_f32(&mut br);
-        println!("xloc: {}", &xloc);
+        writeln!(&mut output, "xloc: {}", &xloc).unwrap();
 
         let yloc: f32 = get_next_as_f32(&mut br);
-        println!("yloc: {}", &yloc);
+        writeln!(&mut output, "yloc: {}", &yloc).unwrap();
 
         br.advance_by(9);
 
         let node_count = get_next_as_u32(&mut br);
-        println!("nodes: {}", &node_count);
+        writeln!(&mut output, "nodes: {}", &node_count).unwrap();
 
         let mut nodes: Vec<PsgNode> = Vec::new();
 
         for _j in 0..node_count {
             let node_id = get_next_as_u32(&mut br);
-            println!("node id: {}", &node_id);
+            writeln!(&mut output, "node id: {}", &node_id).unwrap();
 
             let orbit = get_next_as_u32(&mut br);
-            println!("orbit: {}", &orbit);
+            writeln!(&mut output, "orbit: {}", &orbit).unwrap();
 
             let orbit_index = get_next_as_u32(&mut br);
-            println!("orbit index: {}", &orbit_index);
+            writeln!(&mut output, "orbit index: {}", &orbit_index).unwrap();
 
             let edge_count = get_next_as_u32(&mut br);
-            println!("edge count: {}", &edge_count);
+            writeln!(&mut output, "edge count: {}", &edge_count).unwrap();
 
             let mut edges: Vec<u32> = Vec::new();
 
             for _k in 0..edge_count {
                 let new_edge: u32 = get_next_as_u32(&mut br);
-                println!("new edge: {}", &new_edge);
+                writeln!(&mut output, "new edge: {}", &new_edge).unwrap();
                 edges.push(new_edge);
             }
 
@@ -53,8 +56,8 @@ fn main() {
             };
 
             nodes.push(new_node);
-            println!("node pushed");
-            println!("");
+            writeln!(&mut output, "node pushed").unwrap();
+            writeln!(&mut output, "").unwrap();
         }
 
         let new_group: PsgGroup = PsgGroup {
@@ -64,11 +67,11 @@ fn main() {
         };
 
         groups.push(new_group);
-        println!("group {} pushed", _i+1);
-        println!("");
-        println!("");
+        writeln!(&mut output, "group {} pushed", _i+1).unwrap();
+        writeln!(&mut output, "").unwrap();
+        writeln!(&mut output, "").unwrap();
     }
-    println!("{}", groups.len());
+    writeln!(&mut output, "{}", groups.len()).unwrap();
 }
 
 struct PsgNode {
